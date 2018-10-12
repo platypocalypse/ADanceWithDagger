70af85cf5ba08f65e1a76d4ba2fe205443df0010
Pat <pat.benike@gmail.com>
2018-10-12 16:01:03 -0500

    Added RxJava, applied to reactive RecyclerViewAdapter wrapper

--- app/src/main/java/example/android/com/adancewithdagger/view/recyclerview/adapter/BaseRecyclerAdapter.kt
+++ app/src/main/java/example/android/com/adancewithdagger/view/recyclerview/adapter/BaseRecyclerAdapter.kt
@@ -1,19 +1,65 @@
 package example.android.com.adancewithdagger.view.recyclerview.adapter
 
-import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxrelay2.PublishRelay
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 
-class BaseRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+import java.util.ArrayList
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+abstract class BaseRecyclerAdapter<T, U : RecyclerView.ViewHolder>
+    constructor(private val hasSameId: BiFunction<T, T, Boolean>) : RecyclerView.Adapter<U>() {
+
+    private val values = ArrayList<T>()
+
+    private val clicksRelay = PublishRelay.create<Pair<Int, T>>()
+
+    fun getCloneOfValues(): List<T> {
+        val items = ArrayList<T>()
+        items.addAll(values)
+        return items
     }
+
+    fun getClicks(): Observable<Pair<Int, T>> = clicksRelay
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return values.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun swap(newValues: List<T>) {
+        val diffResult = DiffUtil.calculateDiff(createElementsDiffCallback(newValues), true)
+        values.clear()
+        values.addAll(newValues)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    private fun createElementsDiffCallback(newValues: List<T>): DiffUtil.Callback {
+        return object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int = values.size
+
+            override fun getNewListSize(): Int = newValues.size
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
+                    = hasSameId.apply(values[oldItemPosition], newValues[newItemPosition])
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
+                    = values[oldItemPosition] == newValues[newItemPosition]
+        }
+    }
+
+    override fun onBindViewHolder(holder: U, position: Int) {
+        val element = values[position]
+        RxView.clicks(holder.itemView)
+            .map{ Pair(holder.adapterPosition, element) }
+            .subscribe(clicksRelay)
+        onBindViewHolder(holder, position, element)
+    }
+
+    protected abstract fun onBindViewHolder(holder: U, position: Int, element: T)
+
+    protected fun getPosition(position: Int): T {
+        return values[position]
     }
 }
