39a8caa0392c3f01a72e108327dbb289259d3878
Pat <pat.benike@gmail.com>
2018-10-21 18:39:54 -0500

    Removed unused api call since fuzzy search isn't supported

--- app/src/main/java/example/android/com/adancewithdagger/data/network/ApiService.kt
+++ app/src/main/java/example/android/com/adancewithdagger/data/network/ApiService.kt
@@ -4,14 +4,10 @@ import example.android.com.adancewithdagger.data.model.HouseDto
 import kotlinx.coroutines.experimental.Deferred
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("houses/")
     fun getAllHouses() : Deferred<Response<List<HouseDto>>>
-
-    @GET("houses/")
-    fun getHouses(@Query("name") term: String) : Deferred<Response<List<HouseDto>>>
 
 }
